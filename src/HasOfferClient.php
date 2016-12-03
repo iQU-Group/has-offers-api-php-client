9a8bd4907d11b7f61ce6d1daeae4059f51b7a69b
Evangelos Karvounis <karvounis.b@gmail.com>
2016-12-03 19:52:55 +0100

    Added Advertiser class and the methods

---
+++ src/HasOfferClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Iqu\HasOffersAPIClient;
+
+use Iqu\HasOffersAPIClient\Interfaces\Client;
+
+class HasOfferClient implements Client
+{
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function post()
+    {
+        // TODO: Implement post() method.
+    }
+
+    public function put()
+    {
+        // TODO: Implement put() method.
+    }
+
+    public function delele()
+    {
+        // TODO: Implement delele() method.
+    }
+}
