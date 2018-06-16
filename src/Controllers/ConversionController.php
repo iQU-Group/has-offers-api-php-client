217fe746fbaa419be71d22a4b119874453301d6a
Evangelos Karvounis <evangeloskarvounis@Evangeloss-MacBook-Pro.local>
2018-06-16 10:53:54 +0200

    added 2 methods in conversions controller

--- src/Controllers/ConversionController.php
+++ src/Controllers/ConversionController.php
@@ -29,8 +29,44 @@ class ConversionController extends BaseController
         array $contain = array(),
         $limit = HasOffersConstants::DEFAULT_LIMIT,
         $page = HasOffersConstants::DEFAULT_PAGE_NUMBER
-    ) {
+    )
+    {
         return parent::findAllByTarget(HasOffersConstants::TARGET_CONVERSION, $filters, $sort, $fields, $contain,
             $limit, $page);
+    }
+
+    /**
+     * Create a Conversion.
+     *
+     * @param $data
+     * @param bool $returnObject
+     * @return \Iqu\HasOffersAPIClient\HasOffersResponse
+     */
+    public function create(array $data, $number = 1, $returnObject = true)
+    {
+        $postFields = array(
+            HasOffersConstants::LITERAL_DATA => $data,
+            HasOffersConstants::LITERAL_NUMBER => $number,
+            HasOffersConstants::LITERAL_RETURN_OBJECT => $returnObject
+        );
+        return $this->sendPostRequest(HasOffersConstants::TARGET_CONVERSION,
+            HasOffersConstants::METHOD_CREATE, $postFields);
+    }
+
+    /**
+     * Given a conversion id, updates the status of this conversion.
+     *
+     * @param $id
+     * @param $status
+     * @return \Iqu\HasOffersAPIClient\HasOffersResponse
+     */
+    public function updateStatus($id, $status)
+    {
+        $postFields = array(
+            HasOffersConstants::LITERAL_ID => $id,
+            HasOffersConstants::LITERAL_STATUS => $status
+        );
+        return $this->sendPostRequest(HasOffersConstants::TARGET_CONVERSION,
+            HasOffersConstants::METHOD_UPDATE_STATUS, $postFields);
     }
 }
